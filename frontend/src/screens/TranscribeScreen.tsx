3d25b24d3a662277ff16aaf1634f7f17d1d7e4a3
zhanginc <zhanginc@students.cs.ubc.ca>
2024-01-20 12:46:53 -0800

    adds boiler plate

--- frontend/src/screens/TranscribeScreen.tsx
+++ frontend/src/screens/TranscribeScreen.tsx
@@ -1,24 +1,13 @@
-// transcribe page
 import React from "react";
-import { Button, View, StyleSheet, ScrollView, Image } from "react-native";
-import { RootStackParamList } from "src/types";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from 'react-native';
 
 
-type Props = NativeStackScreenProps<RootStackParamList, "transcribe">;
 
-export default function TranscribeScreen({navigation}: Props) {
-    return (
-        <View style={styles.container}>
-          transcribe
+export const TranscribeScreen = () => {
+    return(
+        <View>
+            
+
         </View>
-
-    )
+    );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: "100%",
-    },
-})
